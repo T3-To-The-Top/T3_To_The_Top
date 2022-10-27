4ca10136344e1c6537edb698782e11192771f7a4
namsh1125 <namsh1125@naver.com>
2022-10-27 22:00:51 +0900

    I need your help

--- Map/Map.js
+++ Map/Map.js
@@ -1,4 +1,8 @@
-import { RandomObjectGeneration } from "../Random_object_generation.module";
+import { RandomObjectGeneration } from "../Random_object_generation.module.js";
+import * as THREE from "../three.js-master/build/three.module.js"
+import { OrbitControls } from "../three.js-master/examples/jsm/controls/OrbitControls.js"
+import { ColladaLoader } from "../three.js-master/examples/jsm/loaders/ColladaLoader.js"
+
 
 // 3차원 세계
 var scene = new THREE.Scene();
@@ -11,15 +15,17 @@ var renderer = new THREE.WebGLRenderer( { antialias: true, preserveDrawingBuffer
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFShadowMap;		// <-- 속도가 빠르다
+renderer.shadowMap.type = THREE.PCFShadowMap;       // <-- 속도가 빠르다
 
 var model;
 
 var loader = new THREE.TextureLoader();
-var loaderMesh = new THREE.ColladaLoader();
+var loaderMesh = new ColladaLoader();
+
+var objects = new RandomObjectGeneration();
 
 // 바닥
-scene.add(new RandomObjectGeneration());
+scene.add(objects.floor);
 
 
 
@@ -27,7 +33,7 @@ scene.add(new RandomObjectGeneration());
 camera.position.set ( 25, 5, 3 );
 
 // 카메라가 회전하는
-var controls = new THREE.OrbitControls (camera, renderer.domElement);
+var controls = new OrbitControls (camera, renderer.domElement);
 controls.enablePan = false;
 controls.minPolarAngle = Math.PI / -2;
 controls.maxPolarAngle = Math.PI / 2.1;
@@ -39,43 +45,12 @@ scene.add( light_base );
 var light_sun = new THREE.DirectionalLight ( 0x808080, 5.0 );
 //light_sun.position.set( 200, 200, 300 );
 scene.add( light_sun );
-shadowBlur=10;
+var shadowBlur=10;
 light_sun.castShadow=true;
 light_sun.shadow.camera.left=-shadowBlur;
 light_sun.shadow.camera.right=shadowBlur;
 light_sun.shadow.camera.top=shadowBlur;
 light_sun.shadow.camera.bottom=-shadowBlur;
-
-// Water
-var waterGeometry = new THREE.PlaneBufferGeometry( 100000, 100000 );
-
-water = new THREE.Water(
-    waterGeometry,
-    {
-        textureWidth: 512,
-        textureHeight: 512,
-        waterNormals: new THREE.TextureLoader().load( 'http://dreamplan7.cafe24.com/canvas/img/waternormals.jpg', function ( texture ) {
-            texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-        } ),
-        alpha: 1.0,
-        sunDirection: light_sun.position.clone().normalize(),
-        sunColor: 0xffffff,
-        waterColor: 0x001e0f,
-        distortionScale: 3.7,
-        fog: scene.fog !== undefined
-    }
-);
-
-water.rotation.x = - Math.PI / 2;
-scene.add( water );
-
-var sky = new THREE.Sky();
-
-sky.material.uniforms['turbidity'].value=10;
-sky.material.uniforms['rayleigh'].value=2;
-sky.material.uniforms['luminance'].value=1;
-sky.material.uniforms['mieCoefficient'].value=0.005;
-sky.material.uniforms['mieDirectionalG'].value=0.8;
 
 var parameters = {
     distance: 400,
@@ -93,11 +68,6 @@ light_sun.position.x = parameters.distance * Math.cos( phi );
 light_sun.position.y = parameters.distance * Math.sin( phi ) * Math.sin( theta );
 light_sun.position.z = parameters.distance * Math.sin( phi ) * Math.cos( theta );
 
-sky.material.uniforms['sunPosition'].value = light_sun.position.copy( light_sun.position );
-water.material.uniforms['sunDirection'].value.copy( light_sun.position ).normalize();
-
-cubeCamera.update( renderer, sky );
-
 // ==========================
 // 초기화 부분 끝
 // ========================== 
@@ -111,11 +81,9 @@ var animate = function () {
         requestAnimationFrame(animate); 
     }, 1000 / framesPerSecond);
 
-    water.material.uniforms[ 'time' ].value += 1.0 / 60.0;
-
     // 랜더링을 수행합니다.
     renderer.render( scene, camera );
 };
 
 // animate()함수를 최초에 한번은 수행해주어야 합니다.
-animate();	
+animate();  
--- Random_object_generation.module.js
+++ Random_object_generation.module.js
@@ -2,29 +2,19 @@ import * as THREE from '../three.js-master/build/three.module.js';
 import { TransformControls } from '../three.js-master/examples/jsm/controls/TransformControls.js';
 import Stats from '../three.js-master/examples/jsm/libs/stats.module.js';
 import { Flow } from '../three.js-master/examples/jsm/modifiers/CurveModifier.js';
+import { Object3D } from './three.js-master/build/three.module.js';
 
 export class RandomObjectGeneration {
 
     constructor(){
 
         // 바닥
-        var floor = new THREE.Mesh(
+        this.floor = new Object3D( new THREE.Mesh(
             new THREE.BoxGeometry(10, 10, 10)
-        );
+        ));
 
-        loader.load(
-            'http://dreamplan7.cafe24.com/canvas/img/floor1.jpg', 
-            function ( texture ) {
-                floor.material = new THREE.MeshStandardMaterial({map: texture});
-                floor.material.map.repeat.x=3;
-                floor.material.map.repeat.y=3;
-                floor.material.map.wrapS=THREE.RepeatWrapping;
-                floor.material.map.wrapT=THREE.RepeatWrapping;
-            }    
-        );
-
-        floor.position.set(0, 50, 0);
-        floor.receiveShadow=true;
+        this.floor.position.set(0, 50, 0);
+        this.floor.receiveShadow=true;
 
     }
 }
