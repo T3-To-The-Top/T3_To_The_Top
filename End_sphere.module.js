280fcd8b03f6aec8a2e9e1b22fa171fcf7ff702e
CodingNiceTry <rdstar2972@gmail.com>
2022-11-03 10:50:13 +0900

    Final Map Gui

--- End_sphere.module.js
+++ End_sphere.module.js
@@ -7,8 +7,8 @@ export class EndSphere {
 
     constructor(radius, posX, posY, posZ, texture) {
         this.geometry = new THREE.SphereGeometry(radius);
-
-        this.material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+        this.loader = new THREE.TextureLoader();
+        this.material = new THREE.MeshBasicMaterial({ color: 0x33FFFF });
 
         this.end = new THREE.Mesh(this.geometry, this.material);
 
